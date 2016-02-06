8b7e01c498ab2b483a54b78c2a7c76bc28a54926
Lance Rogers <lancekincaid1994@gmail.com>
2016-02-06 06:25:58 -0500

    working on some python

--- app/src/main/java/com/lancekrogers/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/lancekrogers/stormy/ui/HourlyForecastActivity.java
@@ -15,18 +15,8 @@ public class HourlyForecastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
--- app/src/main/java/com/lancekrogers/stormy/ui/MainActivity.java
+++ app/src/main/java/com/lancekrogers/stormy/ui/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String DAILY_FORECAST = "DAILY_FORECAST";
+    public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
 
     private Forecast mForecast;
 
@@ -425,6 +426,13 @@ public class MainActivity extends AppCompatActivity
         intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
         startActivity(intent);
     }
+    @OnClick (R.id.hourlyButton)
+    public void startHourlyActivity(View view) {
+        Intent intent = new Intent(this, DailyForcastActivity.class);
+        intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
+        startActivity(intent);
+    }
+
 
 
     @Override
